37304cf81c660b61f9c0bac9930b9b0e9a3d1734
uezu <uezukatsushi@yahoo.co.jp>
2021-04-25 14:29:33 +0000

    8章8.1.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,21 @@ class UsersController < ApplicationController
    end
  end
  
- private
+  def edit
+  @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合の処理を記述します。
+    else
+      render :edit
+    end
+  end
+      
+private
+ 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
