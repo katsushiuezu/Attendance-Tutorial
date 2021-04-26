5e61828c82f959bbd9a0b7f3c3a489ed542317c2
uezu <uezukatsushi@yahoo.co.jp>
2021-04-26 13:04:53 +0000

    8.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -22,21 +23,36 @@ class UsersController < ApplicationController
  end
  
   def edit
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
   
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功した場合の処理を記述します。
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
     else
       render :edit
     end
   end
       
-private
+  private
  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+  # beforeフィルター
+
+    # ログイン済みのユーザーか確認します。
+    
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+    end
+  end
 end
+     
+        
+      
